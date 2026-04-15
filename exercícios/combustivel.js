57e0d6ef25ffa80b9c52653abdf6ca71fb49faac
RAFAELA DE OLIVEIRA QUINTILIANO <rafaela.oquintiliano@senacsp.edu.br>
2026-04-14 22:23:03 -0300

    exercicios

---
+++ exercícios/combustivel.js
@@ -0,0 +1,18 @@
+
+/**
+ * Calculo do consumo de combustvel
+ * @author Rafaela Quintiliano
+ */
+
+
+const prompt = require (`prompt-sync`)()
+
+//variaveis
+let distancia,litros
+console.log ("Valor do consumo  do combustível")
+
+//entrada
+distancia = Number (prompt("Digite a distância percorrida:    "))
+litros = Number (prompt("Digite quantos litros con"))
+
+//processamento
