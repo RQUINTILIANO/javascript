04ed8535054ddfa0c9c40a4eafcca155c40a031c
rafaela quintiliano <rafaela.quintilianoo@outlook.com>
2026-04-27 22:27:37 -0300

    jokenpo

--- exercícios/jokenpo.js
+++ exercícios/jokenpo.js
@@ -10,70 +10,73 @@ const prompt = require('prompt-sync')()
 let jogador, computador
 
 //UX
-console.clear()
-console.log("Teste//")
+do {
+    console.clear()
+    console.log("Teste//")
 
-console.log("       __    ______    __  ___  _______ .__   __. .______     ______   ")
-console.log("      |  |  /  __  \  |  |/  / |   ____||  \ |  | |   _  \   /  __  \  ")
-console.log("      |  | |  |  |  | |  '  /  |  |__   |   \|  | |  |_)  | |  |  |  | ")
-console.log(".--.  |  | |  |  |  | |    <   |   __|  |  . `  | |   ___/  |  |  |  | ")
-console.log("|  `--'  | |  `--'  | |  .  \  |  |____ |  |\   | |  |      |  `--'  | ")
-console.log(" \______/   \______/  |__|\__\ |_______||__| \__| | _|       \______/  ")
-console.log("")
+    console.log("       __    ______    __  ___  _______ .__   __. .______     ______   ")
+    console.log("      |  |  /  __  \  |  |/  / |   ____||  \ |  | |   _  \   /  __  \  ")
+    console.log("      |  | |  |  |  | |  '  /  |  |__   |   \|  | |  |_)  | |  |  |  | ")
+    console.log(".--.  |  | |  |  |  | |    <   |   __|  |  . `  | |   ___/  |  |  |  | ")
+    console.log("|  `--'  | |  `--'  | |  .  \  |  |____ |  |\   | |  |      |  `--'  | ")
+    console.log(" \______/   \______/  |__|\__\ |_______||__| \__| | _|       \______/  ")
+    console.log("")
 
-//Lógica do jogador
+    //Lógica do jogador
 
-console.log("1.Pedra")
-console.log("2.Papel")
-console.log("3.Tesoura")
-jogador = Number(prompt("Digite aopção desejada:  "))
-console.log("")
+    console.log("1.Pedra")
+    console.log("2.Papel")
+    console.log("3.Tesoura")
+    jogador = Number(prompt("Digite aopção desejada:  "))
+    console.log("")
 
-switch (jogador) {
-    case 1:
-        console.log("Jogador escolheu pedra")
-        break;
-}
-switch (jogador) {
-    case 1:
-        console.log("Jogador escolheu papel")
-        break;
-}
-switch (jogador) {
-    case 1:
-        console.log("Jogador escolheu Tesoura")
-        break;
-    default:
-        console.log("Opção Invalida")
-        break;
-}
+    switch (jogador) {
+        case 1:
+            console.log("Jogador escolheu pedra")
+            break;
+    }
+    switch (jogador) {
+        case 1:
+            console.log("Jogador escolheu papel")
+            break;
+    }
+    switch (jogador) {
+        case 1:
+            console.log("Jogador escolheu Tesoura")
+            break;
+        default:
+            console.log("Opção Invalida")
+            break;
+    }
 
-//  Lógica do computador 
+    //  Lógica do computador 
 
-//
-computador = Math.ceil(Math.random() * 3) // 1, 2 ou 3
+    //
+    computador = Math.ceil(Math.random() * 3) // 1, 2 ou 3
 
-switch (computador) {
-    case 1:
-        console.log("Computador escolheu pedra")
-        break;
-}
-switch (jogador) {
-    case 1:
-        console.log("Computador escolheu papel")
-        break;
-}
-switch (jogador) {
-    case 1:
-        console.log("Computador escolheu Tesoura")
-        break;
-}
+    switch (computador) {
+        case 1:
+            console.log("Computador escolheu pedra")
+            break;
+    }
+    switch (jogador) {
+        case 1:
+            console.log("Computador escolheu papel")
+            break;
+    }
+    switch (jogador) {
+        case 1:
+            console.log("Computador escolheu Tesoura")
+            break;
+    }
 
-//Lógica para determinar o vencedor ou declarar empate
-if (jogador === computador)
-    console.log("EMPATE")
-else if (jogador === 1 && computador === 3) || (jogador === 2 && computador === 1) || (jogador === 3 && computador === 2){
-    console.log("Jogador venceu")
-} else  {
-    console.log("Computador venceu")
-}
+    //Lógica para determinar o vencedor ou declarar empate
+    if (jogador === computador) {
+        console.log("EMPATE")
+    } else if ((jogador === 1 && computador === 3) || (jogador === 2 && computador === 1) || (jogador === 3 && computador === 2)) {
+        console.log ("Jogador venceu")
+    } else {
+        console.log("Computador venceu")
+    }
+    novoJogo = prompt ("Deseja jogar novamente (s/n)")
+} while (novoJogo === "s" || novoJogo === "s") 
